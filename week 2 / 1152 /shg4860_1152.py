40c06435cf65e5f2e662cfa50313f217d453b90f
HeeKyung <shg4860@khu.ac.kr>
2026-03-28 14:05:26 +0900

    단어수세기
    Count the number of spaces in the input sentence and print the result.

---
+++ week 2 / 1152 /shg4860_1152.py
@@ -0,0 +1,15 @@
+sentence = input().strip() # 양 옆 공백 삭제
+count = 0 
+
+
+if sentence == '': # "   " 입력 시 
+    print(count)
+
+else : 
+    for i in sentence : 
+        if i == ' ' : 
+            count += 1
+    print(count+1)
+
+
+#strip은 원래 변수의 값을 바꾸지 않고 리턴만 함 
