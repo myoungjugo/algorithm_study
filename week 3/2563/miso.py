759e60e5bdb16b4f1541dd8d785d4377942c4822
parkmiso-smile <misowendy04@khu.ac.kr>
2026-04-06 22:18:55 +0900

    Add miso.py to count filled cells in a matrix
    Implement a program to count filled cells in a 100x100 matrix based on user input.

---
+++ week 3/2563/miso.py
@@ -0,0 +1,19 @@
+num =int( input())
+#100*100의 행렬을 초기화하는 방법 
+
+#지원짱 희경짱의 아이디어를 살짝쿵 참고했어요 >.< 무한감사
+
+lis=[[0]*100 for _ in range(100)]
+
+for _ in range(num):
+    x,y= map(int, input().split())
+    for col in range(x,x+10):
+        for row in range(y,y+10):
+            lis[col][row]=1
+    
+sum=0
+for i in range(100):
+    k=lis[i]
+    sum+=k.count(1)
+
+print(sum)
