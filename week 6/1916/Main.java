8969ae623a8b603e6fba70163d3b00ac264053aa
minseokim <80013889+minse0kim@users.noreply.github.com>
2026-05-19 15:00:23 +0900

    kimminseo1916

---
+++ week 6/1916/Main.java
@@ -0,0 +1,38 @@
+package org.codeup;
+
+
+import java.util.Scanner;
+
+public class Main {
+    // 1. N은 최대 200이므로 201 크기의 static 배열을 선언합니다.
+    // 자바가 알아서 모든 칸을 0으로 초기화해줌
+    static int[] memo = new int[201];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            System.out.println(fibonacci(n));
+        }
+
+        sc.close();
+    }
+
+    public static int fibonacci(int n) {
+        // Base Case: 1번째와 2번째 피보나치 수는 무조건 1
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        // 2. 이미 계산한 적이 있는 값이면 (0이 아니라면) 저장된 값을 바로 반환!
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+
+        // 3. 계산한 적이 없다면 재귀로 구한 뒤, 배열에 저장하고 반환 (10009 나머지 연산 필수)
+        memo[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % 10009;
+
+        return memo[n];
+    }
+}
