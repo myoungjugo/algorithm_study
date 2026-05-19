39011c4a28b1f21af652cf2487f77df17951b10b
ddingju <rinaroo0512@naver.com>
2026-05-19 10:52:42 +0900

    Update myoungju.cpp

--- week 6/1916/myoungju.cpp
+++ week 6/1916/myoungju.cpp
@@ -9,6 +9,8 @@ using namespace std;
 //-> 아님. 어차피 그 안에서 재귀 계속 들어가다가 결국 배열에서 꺼내서 리턴해야 하니까
 //리턴값 일단 long int로 해봄
 
+//피보나치 수는 n이 90만 증가해도 c++에서 가장 큰 정수 자료형인 unsigned long long 범위 초과.
+
 long int fibo(int n, long int cache[]){
 	if(n<=2) return 1; //base case, 종료조건
 	
@@ -16,17 +18,22 @@ long int fibo(int n, long int cache[]){
 	if(cache[n]!=0){//이미 계산한거면
 		return cache[n];
 	}
-	cache[n] = (fibo(n-1, cache) + fibo(n-2, cache)) %  10009;
+	cache[n] = (fibo(n-1, cache) + fibo(n-2, cache)) %  10009; //오버플로우 나지 않게 애초에 %10009해서 저장
+
+	//Q : 10009 나눈 나머지 저장해 더하면 나중에 그걸 꺼내 쓸 때 값이 오염되는 것 아닌가
+	//A : 큰 수 두 개 더한 다음에 10009로 나누나, 애초에 10009로 나눈 나머지들끼리 더한 다음 다시 10009로 나누나 결과 무조건 같음
+	//(A + B) mod C = ((A mod C) + (B mod C)) mod C
+	
 	return cache[n];
 }
 
 int main(){
 	int n;
-	int ret;
+	
 	long int cache[201] = {0};
 	cin >> n;
-	ret = fibo(n, cache);
-	cout << ret;
+
+	cout << fibo(n, cache);
 	return 0;
 	
 	
